e342884811f41ae9b3cd4bcf332ef63decf6fec6
Har <Har@MacBook-Pro.local>
2017-08-28 09:52:10 +1000

    test

---
+++ main1.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  20003Proj1Stage2
+//
+//  Created by Har on 27/8/17.
+//  Copyright © 2017 Hardaya Singh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
